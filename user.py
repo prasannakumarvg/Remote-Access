22722682faad757e0abd388e9a67c22ee934bd5d
prasannakumarvg <123349921+prasannakumarvg@users.noreply.github.com>
2023-12-14 21:56:30 +0530

    Update user.py

--- user.py
+++ user.py
@@ -19,84 +19,92 @@ def Main():
             print("Connection closed by server.")
             break
 
-        if command.lower() == "ls":
+        elif command.lower() == "ls":
             result = os.popen('ls').read()
             s.send(result.encode())
 
-        if command.lower() == "hidden":
+        elif command.lower() == "hidden":
             result = [item for item in os.listdir('.') if item.startswith('.')]
             resultj=json.dumps(result)
             s.send(resultj.encode())
 
 
-        if command.lower() == "cd ../":
+        elif command.lower() == "cd ../":
             os.chdir("../")
             result = "Directory changed successfully."
             s.send(result.encode())
 
-        if command.lower() == "rm.file":
+        elif command.lower() == "rm.file":
             file = s.recv(1024).decode()
             os.remove(file)
             result = f"File '{file}' deleted successfully."
             s.send(result.encode())
 
-        if command.lower() == "cwd":
+        elif command.lower() == "cwd":
             result = os.getcwd()
             s.send(result.encode())
 
-        if command.lower() == "mkdir":
+        elif command.lower() == "mkdir":
             dire = s.recv(1024).decode()
             os.makedirs(os.path.join(os.path.expanduser("~"), dire), exist_ok=True)
             result = f"Directory '{dire}' created successfully."
             s.send(result.encode())
 
-        if command.lower() == "cd":
+        elif command.lower() == "cd":
             change = s.recv(1024).decode()
             os.chdir(change)
             result = f"Directory '{change}' changed Successfully."
             s.send(result.encode())
 
 
-        if command.lower() == "rm.dir":
+        elif command.lower() == "rm.dir":
             directory = s.recv(1024).decode()
             shutil.rmtree(directory)
             result = f"Directory '{directory}' deleted successfully."
             s.send(result.encode())
 
-        if command.lower()== "file":
+        elif command.lower()== "file":
             name = s.recv(1024).decode()
             open(name, 'w')
             result = "File created sucessfully."
             s.send(result.encode())
 
-        if command.lower() == "copy":
+        elif command.lower() == "copy":
             src = s.recv(1024).decode()
             des = s.recv(1024).decode()
             shutil.copy(src, des)
             result = "File copied sucessfully. "
             s.send(result.encode())
             
-        if command.lower() == "move":
+        elif command.lower() == "move":
             src = s.recv(1024).decode()
             des = s.recv(1024).decode()
             shutil.move(src, des)
             result = "File moved sucessfully. "
             s.send(result.encode())
         
-        if command.lower()=="open":
+        elif command.lower()=="open":
             file = s.recv(1024).decode()
             file_path = os.path.abspath(file)
             subprocess.run(["xdg-open", file_path], check=True)
             result = "Opened Sucessfully."
             s.send(result.encode())
 
-        if command.lower()=="rename":
+        elif command.lower()=="rename":
             src = s.recv(1024).decode()
             des = s.recv(1024).decode()
             os.rename(src, des)
             result = "Name changed Successfully. "
             s.send(result.encode())
-    
+        
+        elif command.lower()=="help":
+             result="Empty"
+             s.send(result.encode())
+
+        else:
+            result="Invalid Input "
+            s.send(result.encode())
+
     
     
     except KeyboardInterrupt:
